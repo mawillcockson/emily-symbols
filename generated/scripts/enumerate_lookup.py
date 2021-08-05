c4e794634c96d60fdf78a04c5d1f308543b335b0
Matthew Willcockson <matthew@willcockson.family>
2021-08-05 07:46:27 -0500

    ci(enumerate): fill out argument parsing

--- generated/scripts/enumerate_lookup.py
+++ generated/scripts/enumerate_lookup.py
@@ -1,25 +1,51 @@
 #!/usr/bin/env python
 """
-generates json (and optionally RTF/CRE dictionaries, if sammdot/rtfcre is
-installed) stenographic dictionaries of Emily's system for use with
+generates json stenographic dictionaries of Emily's system for use with
 openstenoproject/plover
 """
 import argparse
 import json
-from argparse import Namespace
+import sys
 from itertools import compress
 from pathlib import Path
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import Optional, Sequence, Iterator, List, Dict
 
 import emily_symbols
 
+
+def str_to_int(string: str) -> int:
+    "makes mypy happy"
+    if string.isdigit():
+        return int(string)
+
+    raise ValueError(f"not a number: {string}")
+
+
 pattern_keys = "FRPBLG"
-patterns = [
-    "".join(compress(pattern_keys, map(int, f"{x:0{len(pattern_keys)}b}")))
-    for x in range(2 ** len(pattern_keys))
-]
+patterns: "List[str]" = []
+for number in range(1 << len(pattern_keys)):
+    # same as 2 ** len(pattern_keys)
+
+    selectors: "List[int]" = []
+    for binary_digit in f"{number:0{len(pattern_keys)}b}":
+        if binary_digit == "1":
+            selectors.append(1)
+        else:
+            selectors.append(0)
+
+    patterns.append("".join(compress(pattern_keys, selectors)))
+
+# same as
+# patterns: "List[str]" = [
+#     "".join(compress(pattern_keys, map(int, f"{x:0{len(pattern_keys)}b}")))
+#     for x in range(2 ** len(pattern_keys))
+# ]
 
 
-def generate_combinations():
+def generate_combinations() -> "Iterator[str]":
     # vary uniqueStarters
     for starter in emily_symbols.symbols:
         # detect no custom symbols
@@ -44,6 +70,7 @@ def generate_combinations():
 
 key_to_number = str.maketrans(
     {
+        "#": None,
         "S": "1",
         "T": "2",
         # "P": "3",
@@ -83,11 +110,12 @@ def embed_numbers(outline: str) -> str:
         raise ValueError(f"too many 'P's in outline '{outline}'")
 
 
-def parse_args(args: "Optional[Sequence[str]]" = None) -> "Namespace":
+def parse_args(args: "Optional[Sequence[str]]" = None) -> "Dict[str, str]":
+    "parses arguments"
     parser = argparse.ArgumentParser(
         description=(
-            "generates json (and optionally RTF/CRE dictionaries, if sammdot/rtfcre is installed) "
-            "stenographic dictionaries of Emily's system for use with openstenoproject/plover"
+            "generates json stenographic dictionaries of Emily's "
+            "system for use with openstenoproject/plover"
         )
     )
 
@@ -98,21 +126,21 @@ def parse_args(args: "Optional[Sequence[str]]" = None) -> "Namespace":
         action="store",
         choices=["space", "attachment"],
         default=emily_symbols.attachmentMethod,
-        help=f"whether the spacing symbols indicate which sides to put spaces on, or which sides to attach to (default is {emily_symbols.attachmentMethod})",
-    )
-    regular_group.add_argument(
-        "--rtfcre",
-        dest="rtfcre",
-        action="store_true",
-        default=False,
-        help="also generate RTF/CRE dictionaries",
+        help=(
+            "whether the spacing symbols indicate which sides to put "
+            "spaces on, or which sides to attach to "
+            f"(default is {emily_symbols.attachmentMethod})"
+        ),
     )
     regular_group.add_argument(
         "--include-embedded-numbers",
-        dest="embed_numbers",
+        dest="include_embedded_numbers",
         action="store_true",
         default=False,
-        help="include duplicate entries for outlines with the numbers embedded (e.g. both #SKWH and 1KW4 are included)",
+        help=(
+            "include duplicate entries for outlines with the numbers embedded "
+            "(e.g. both #SKWH and 1KW4 are included)"
+        ),
     )
     regular_group.add_argument(
         "--directory",
@@ -120,7 +148,10 @@ def parse_args(args: "Optional[Sequence[str]]" = None) -> "Namespace":
         action="store",
         default=".",
         required=False,
-        help="directory to put generated dictionaries in (default is the current directory)",
+        help=(
+            "directory to put generated dictionaries in "
+            "(default is the current directory)"
+        ),
     )
 
     ci_group = parser.add_argument_group(
@@ -134,28 +165,75 @@ def parse_args(args: "Optional[Sequence[str]]" = None) -> "Namespace":
         help="generate all possible JSON dictionaries",
     )
 
-    return parser.parse_args(args)
+    return vars(parser.parse_args(args))
 
 
-if __name__ == "__main__":
+def main() -> "None":
+    "parses arguments and generates dictionaries"
     args = parse_args()
+    attachment_method = str(args["attachment_method"])
+    include_embedded_numbers = bool(args["include_embedded_numbers"])
+    directory = Path(args["directory"])
+    json_all = bool(args["json_all"])
+
+    assert directory.is_dir(), f"'{directory}' is not a directory or does not exist"
+
+    if json_all or attachment_method == "space":
+        space_dictonary_path = directory / f"emily-symbols-space.json"
+        assert (
+            not space_dictonary_path.exists() or space_dictonary_path.is_file()
+        ), f"'{space_dictonary_path}' exists and is not a file"
+
+    if json_all or attachment_method == "attachment":
+        attachment_dictionary_path = directory / f"emily-symbols-attachment.json"
+        assert (
+            not attachment_dictionary_path.exists()
+            or attachment_dictionary_path.is_file()
+        ), f"'{attachment_dictionary_path}' exists and is not a file"
 
     space_dictionary = {}
     attachment_dictionary = {}
     for outline in generate_combinations():
-        emily_symbols.attachmentMethod = "space"
-        try:
-            space_dictionary[outline] = emily_symbols.lookup((outline,))
-        except KeyError:
-            pass
+        if json_all or attachment_method == "space":
+            emily_symbols.attachmentMethod = "space"
+            try:
+                space_dictionary[outline] = emily_symbols.lookup((outline,))
+            except KeyError:
+                pass
 
-        emily_symbols.attachmentMethod = "attachment"
-        try:
-            attachment_dictionary[outline] = emily_symbols.lookup((outline,))
-        except KeyError:
-            pass
+            if json_all or include_embedded_numbers:
+                embedded_outline = embed_numbers(outline)
+                try:
+                    space_dictionary[embedded_outline] = emily_symbols.lookup(
+                        (embedded_outline,)
+                    )
+                except KeyError:
+                    pass
 
-    Path("emily-symbols-space.json").write_text(json.dumps(space_dictionary, indent=2))
-    Path("emily-symbols-attachment.json").write_text(
-        json.dumps(attachment_dictionary, indent=2)
-    )
+        if json_all or attachment_method == "attachment":
+            emily_symbols.attachmentMethod = "attachment"
+            try:
+                attachment_dictionary[outline] = emily_symbols.lookup((outline,))
+            except KeyError:
+                pass
+
+            if json_all or include_embedded_numbers:
+                embedded_outline = embed_numbers(outline)
+                try:
+                    attachment_dictionary[embedded_outline] = emily_symbols.lookup(
+                        (embedded_outline,)
+                    )
+                except KeyError:
+                    pass
+
+    if space_dictionary:
+        space_dictonary_path.write_text(json.dumps(space_dictionary, indent=2))
+
+    if attachment_dictionary:
+        attachment_dictionary_path.write_text(
+            json.dumps(attachment_dictionary, indent=2)
+        )
+
+
+if __name__ == "__main__":
+    main()
